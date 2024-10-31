077fc177f967ed7f6479628371abecee5a64ce66
Andrejs Nikolajevs <algebronik228@gmail.com>
2024-10-31 23:01:34 +0200

    gatavs kalkulators

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const screenDisplay = document.querySelector('.screen')
 const buttons = document.querySelectorAll('button')
-console.log(screenDisplay)
-// 17:52 taimcode
+
+let calculation = []
+let accumulativeCalculation
+
+function calculate(button) {
+    const value = button.textContent
+    if (value === "CLEAR") {
+        calculation = []
+        screenDisplay.textContent = '.'
+    } else if (value === '=') {
+        console.log(accumulativeCalculation)
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
